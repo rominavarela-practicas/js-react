397e1a3c3ea0dacbafb9f6497ae97deef56c9f73
romina.espinosa <romina.espinosa@wizeline.com>
2016-08-22 17:14:09 -0500

    static nav

--- dev/js/index.jsx
+++ dev/js/index.jsx
@@ -1,16 +1,14 @@
+import '../style/style.css'
 import React, { Component } from 'react';
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 
-import Container from './container.jsx';
 import CreateView from './views/create.jsx';
 import AllView from './views/all.jsx';
 
 render(
   <Router history={hashHistory}>
-    <Route path='/' component={Container}>
-      <Route path="create" component={CreateView}/>
-      <Route path="all" component={AllView}/>
-    </Route>
+    <Route path="create" component={CreateView}/>
+    <Route path="all" component={AllView}/>
   </Router>
-, document.getElementById('content'))
+, document.getElementById('content'));
